886717e74277cb66555277259f787b310e02ce6e
Albert Strange <46588333+ads6495@users.noreply.github.com>
2019-10-13 20:18:05 -0400

    fixed JS
    functioanlity, almost done

--- public/main.js
+++ public/main.js
@@ -1,162 +1,192 @@
+import { create } from "domain"
+
 const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 const ranks = [
-  'Ace',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  'Jack',
-  'Queen',
-  'King'
+{name: '1', value: 11},
+{name: '2', value: 11},
+{name: '3', value: 11},
+{name: '4', value: 11},
+{name: '5', value: 11},
+{name: '6', value: 11},
+{name: '7', value: 11},
+{name: '8', value: 11},
+{name: '9', value: 11},
+{name: '10', value: 11},
+{name: 'Jack', value: 11},
+{name: 'Queen', value: 11},
+{name: 'King', value: 11}
 ]
-const values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+
 const deck = []
 const playerHand = []
 const dealerHand = []
-let playerHandValue = 0
-let dealerHandValue = 0
 
-class Card {
-  constructor(rank, suit, value) {
-    this.rank = rank
-    this.suit = suit
-    this.value = value
-  }
-}
+
 //create deck
 const createDeck = () => {
-  suits.forEach(suit => {
-    ranks.forEach((rank, index) => {
-      const value = values[index]
-      deck.push(new Card(rank, suit, value))
+deck = []
+suits.forEach(suit => {
+  ranks.forEach(rank => {
+    deck.push({
+      rank: rank.name,
+      value: rank.value,
+      suit: suit
     })
   })
+})
+console.log(deck)
 }
 
 //shuffle deck
 const shuffleDeck = () => {
-  deck.forEach(() => {
-    var location1 = Math.floor(Math.random() * deck.length)
-    var location2 = Math.floor(Math.random() * deck.length)
-    var temp = deck[location1]
-    deck[location1] = deck[location2]
-    deck[location2] = temp
-  })
+ for(let i = deck.length - 1; i >= 0; i--){
+  const a = Math.floor(Math.random() * i)
+  const firstArray = deck[i]
+  const secondArray = deck[a]
+  console.log(firstArray)
+  console.log(secondArray)
+ }
+}
+
+const PlayerDrawCard = () => {
+  // take from shuffled deck
+const dealtCard = deck.pop()
+  //add to playerhand
+  playerHand.push(dealtCard)
+
+}
+
+const dealerDrawCard = () => {
+ const dealtCard = deck.pop()
+ dealerHand.push(dealtCard)
 }
 
 const dealPlayerHand = () => {
-  for (let i = 0; i < 1; i++) {
-    const dealtCard = deck.pop()
-    playerHand.push(dealtCard)
+  for (let i = 0; i < 2; i++) {
+    playerDrawCard()
   }
   console.log(playerHand)
 }
 
-const dealDealerHand = () => {
-  for (let i = 0; i < 1; i++) {
-    const dealtCard = deck.pop()
-    dealerHand.push(dealtCard)
-  }
-  console.log(dealerHand)
-}
-
-const hitMeBaby = () => {
+const dealDealer = () => {
   const playerHandValue = document.querySelector('.player-hand-value')
   const playerName = document.querySelector('.player-name')
   const dealerName = document.querySelector('.dealer-name')
-  const dealtCard = deck.pop()
-  playerHand.push(dealtCard)
-  console.log(playerHand)
-
-  // Determine when player busts
-  valueOfPlayerCards()
-  if (playerHandValue.textContent > 21) {
-    playerName.textContent = 'Player Bust!'
-    dealerName.textContent = 'Dealer Wins!'
-  } else if (playerHandValue.textContent === 21) {
-    playerName.textContent = 'Player Wins!'
-    dealerName.textContent = 'Dealer Loses!'
+  getPlayerHandTotal()
+if(playerHandValue.textContent === 21) {
+  playerName.textContent = "player Wins!"
+  dealerName.textContent = "Dealer Wins!"
+  removeButtons()
+} else {
+  for (let i = 0; i < 2; i++){
+    dealerDrawCard()
   }
-  console.log(playerHand)
+}
+console.log(dealerHand)
+}
+const RemoveButtons = () => {
+  document.querySelector('.hit').classList.add('hide')
+  document.querySelector('.stand').classList.add('hide')
+  document.querySelector('.reset').classList.remove('hide')
 }
 
-const hitDealer = () => {
-  if (dealerHandValue < 17) {
-    const dealtCard = deck.pop()
-    dealerHand.push(dealtCard)
-  } else if (dealerHandValue <= 21 && dealerHandValue > playerHandValue) {
-    document.querySelector('.dealer-name').textContent = 'Dealer Wins!'
-  }
 
-  console.log(dealerHand)
+ 
+const getPlayerHandTotal = () => {
+  const playerHandValue = document.querySelector('.player-hand-value')
+  const playerHandTotal = playerHand.reduce((runningTotal, card) => {
+    return runningTotal += card.value
+  }, 0)
+  playerHandValue.textContent = playerHandTotal
+  console.log(playerHandTotal)
 }
 
-const valueOfPlayerCards = () => {
-  let playerHandValue
-  for (let v = 0; v < playerHand.length; v++) {
-    const card = playerHand[v]
+const getDealerHandTotal = () => {
+  const dealerHandValue = document.querySelector('.dealer-hand-value')
+  const playerHandTotal = playerhand.reduce((runningTotal, card) => {
+    return runningTotal + card.value
+  }, 0)
+  playerHandValue.textContent = playerHandTotal
+  console.log(playerHandTotal)
+}
 
-    if (playerHandValue) {
-      playerHandValue += card.value
-    } else {
-      playerHandValue = card.value
+
+//pressing the hit button to get a card
+const playerHit = () => {
+  const playerHandValue = document.querySelector('.player-hand-value')
+  const playerName = document.querySelector('.player-name')
+  const dealerName = document.querySelector('.dealer-name')
+  for(let i =0; i<1; i++){
+    PlayerDrawCard()
+    getPlayerHandTotal()
+    if(playerHandValue > 21) {
+      playerName.textContent = 'player Bust!'
+      dealerName.textContent = 'Dealer wins!'
     }
-    document.querySelector('.player-name').textContent = playerHandValue
-  }
-  console.log(playerHandValue)
-}
-const valueOfDealerCards = () => {
-  let dealerHandValue
-  for (let v = 0; v < dealerHand.length; v++) {
-    const card = dealerHand[v]
-
-    if (dealerHandValue) {
-      dealerHandValue += card.value
-    } else {
-      dealerHandValue = card.value
-    }
-    document.querySelector('.dealer-name').textContent = dealerHandValue
   }
 }
 
-const compareScores = () => {
-  valueOfDealerCards()
+// when player hits stand
+
+const playerStand = () => {
+  const dealerHandValue = document.querySelector('.dealer-hand-value')
+  const playerHandValue = document.querySelector('.player-hand-value')
+  const dealerName = document.querySelector('.dealer-name')
+  const playerName = document.querySelector('.player-name')
+  
+  RemoveButtons()
+
   while (dealerHandValue.textContent < 17) {
-    for (let i = 0; i < 1; i++) {
-      const dealtCard = deck.pop()
-      // add to dealer hand
-      dealerHand.push(dealtCard)
+    for(let i = 0; i < 1; i++){
+      dealerDrawCard()
+      getDealerHandTotal()
+      if (dealerHandValue.textContent > 21) {
+        playerName.textContent = 'Player Wins!'
+        dealerName.textContent = 'Dealer Bust!'
+      } else if (dealerHandValue.textContent > playerHandValue.textContent) {
+        playerName.textContent = 'Player Loses!'
+        dealerName.textContent = 'Dealer Wins!'
+      } else if (dealerHandValue.textContent < playerHandValue.textContent) {
+        playerName.textContent = 'Player Wins!'
+        dealerName.textContent = 'Dealer Loses!'
+      } else if (dealerHandValue.textContent === playerHandValue.textContent) {
+        playerName.textContent = 'Push!'
+        dealerName.textContent = 'Push!'
+      }
+      console.log(dealerHand)
     }
   }
 }
 
-while (dealerHandValue.textContent < 17) {
-  for (let i = 0; i < 1; i++) {
-    const dealtCard = deck.pop()
-    // add to dealer hand
-    dealerHand.push(dealtCard)
-    valueOfDealerCards()
-  }
-}
-const main = () => {
+const resetGame = () => {
+  const playerName = document.querySelector('.player-name')
+  const dleaerName = document.querySelector('.dealer-name')
+  playerHand=[]
+  dealerHand=[]
   createDeck()
   shuffleDeck()
   dealPlayerHand()
-  dealDealerHand()
-  hitMeBaby()
-  hitDealer()
-  valueOfPlayerCards()
-  valueOfDealerCards()
-  compareScores()
+  dealDealer()
+  getPlayerHandTotal()
+  getDealerHandTotal()
+playerName.textContent = 'Player'
+dealerName.textContent = 'Dealer'
+document.querySelector('.hit').classList.remove('hide')
+document.querySelector('.stand').classList.remove('hide')
+document.querySelector('.reset').classList.add('hide')
 }
 
-document.querySelector('#stand').addEventListener('click', hitDealer)
+const main () => {
+  createDeck()
+  shuffleDeck()
+  dealPlayerHand()
+  dealDealer()
+  getPlayerHandTotal()
+  getDealerHandTotal()
+}
 
 document.addEventListener('DOMContentLoaded', main)
-
-document.querySelector('#hitMe').addEventListener('click', hitMeBaby)
+document.querySelector('.hit').addEventListener('click', playerHit)
+document.querySelector('.reset').addEventListener('click', resetGame)
+document.querySelector('.stand').addEventListener('click', playerStand)
